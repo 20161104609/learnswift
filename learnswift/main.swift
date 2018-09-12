cef1edbf47cd768b9e7e999208538719ac6b7d79
20161104609 <a20161104609@admindeiMac-3.local>
2018-09-12 11:10:05 +0800

    1 jia  100

---
+++ learnswift/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  learnswift
+//
+//  Created by 20161104609 on 18/9/5.
+//  Copyright © 2018年 20161104609. All rights reserved.
+//
+
+import Foundation
+
+var a=1
+a = 10
+var b=3
+var c = a+b
+var sum = 0
+for index in 1...100{
+    
+    sum=sum+index;
+
+}
+/*let  mian = " asdasdasdwqrjlasnfa"
+var ar = [""]
+if  a < ar
+{
+    
+}
+print(mian)*/
+print(sum)
+
+

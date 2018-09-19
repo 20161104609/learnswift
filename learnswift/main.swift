a106e0532f75205a8b0c980f2bd51920a6b049bc
20161104609 <a20161104609@admindeiMac-3.local>
2018-09-19 11:45:02 +0800

    .

--- learnswift/main.swift
+++ learnswift/main.swift
@@ -7,7 +7,9 @@
 //
 
 import Foundation
-//1
+//1       
+
+ 
 var a=1
 a = 10
 var b=3
@@ -25,10 +27,17 @@ var  adc = ("1","2","3","4","5","6","7","8","9","10")
 print(sum)
 
 
-
-
-
 //3
 var ab = ["2","1","3","7","6","0","4","9","8"]
 let t = ab.sorted(by: <)
 print("\(t)")
+
+/*var userList = [UserInfo]()
+userList.append(UserInfo(name: "张三", phone: "4234"))
+userList.append(UserInfo(name: "李四", phone: "1212"))
+userList.append(UserInfo(name: "航歌", phone: "3525"))*/
+var yuan0: (name:String,age:Int,sex:String) = (name:"黄坚鑫", age:27, sex: "男")
+var yuan1 : (name:String,age:Int,sex:String) = (name:"罗锦斌", age:24, sex: "男")
+var yuan2 : (name:String,age:Int,sex:String) = (name:"詹佳妮", age:23, sex: "男")
+var yuan3 : (name:String,age:Int,sex:String) = (name:"林晓荣", age:29, sex: "男")
+var yuan4 : (name:String,age:Int,sex:String) = (name:"陈泽斌", age:19, sex: "男")
